720cb74d4ed9f66a65febf46d6e5840f9bb10620
Davi Alexandre <davialexandre_dap@hotmail.com>
2020-06-05 17:23:42 -0300

    P_add:Calc_Offset_Mag

--- Texas-MSP432-CCS/Peripherals_drivers/main.c
+++ Texas-MSP432-CCS/Peripherals_drivers/main.c
@@ -50,6 +50,12 @@ double tempo_processamento = 0;
 /* For Debug graph*/
 uint8_t RXData[2];
 uint8_t leitura[14];
+float magCalibration[3];
+float magValue[3];
+float magbias[3];
+#define mRes 10.*4912./32760.0
+
+float Magnetometer_offset[3];
 
 
 int DR_angles_update(uint16_t n_sensor)
@@ -94,8 +100,8 @@ void interrupt_angles(){
     DR_tick_start();
     // Leitura do Acelerêometro e Giroscopio
 	DR_i2c_readraw(0,0x68,0x3B,14,leitura);
-	sensor_rho.ax =		(leitura[0] << 8) | (leitura[1]);
-	sensor_rho.ay = 	(leitura[2] << 8) | (leitura[3]);
+	sensor_rho.ax =		(leitura[0] << 8) | (leitura[1]); // AccXH XL
+	sensor_rho.ay = 	(leitura[2] << 8) | (leitura[3]); 
 	sensor_rho.az = 	(leitura[4] << 8) | (leitura[5]);
 	sensor_rho.temp =  	(leitura[6] << 8) | (leitura[7]);
 	sensor_rho.gx = 	(leitura[8] << 8) | (leitura[9]);
@@ -103,13 +109,23 @@ void interrupt_angles(){
 	sensor_rho.gz = 	(leitura[12] << 8) | (leitura[13]);
 	// Leitura do Magnetometro
 	DR_i2c_readraw(0,0x0C,0x03,7,leitura);
-	sensor_theta1.ax =		(leitura[0] << 8) | (leitura[1]);
-	sensor_theta1.ay = 		(leitura[2] << 8) | (leitura[3]);
-	sensor_theta1.az = 		(leitura[4] << 8) | (leitura[5]);
-	sensor_theta1.temp =  	(leitura[6] << 8) | (leitura[7]);
-	sensor_theta1.gx = 		(leitura[8] << 8) | (leitura[9]);
-	sensor_theta1.gy =		(leitura[10] << 8) | (leitura[11]);
-	sensor_theta1.gz = 		(leitura[12] << 8) | (leitura[13]);
+	sensor_theta1.ax =		(leitura[1] << 8) | (leitura[0]); // HXL HXH
+	sensor_theta1.ay = 		(leitura[3] << 8) | (leitura[2]); 
+	sensor_theta1.az = 		(leitura[4] << 8) | (leitura[4]);
+	sensor_theta1.identification = leitura[7];	// Dado pronto 
+	// // Calculo do magnetômetro
+	// magbias[0] = +470.f;  // User environmental x-axis correction in milliGauss, should be automatically calculated
+    // magbias[1] = +120.f;  // User environmental x-axis correction in milliGauss
+    // magbias[2] = +125.f;  // User environmental x-axis correction in milliGauss
+
+	// magValue[0] = (float)sensor_theta1.ax*mRes*magCalibration[0] - magbias[0];
+	// magValue[1] = (float)sensor_theta1.ay*mRes*magCalibration[1] - magbias[1];
+	// magValue[2] = (float)sensor_theta1.az*mRes*magCalibration[2] - magbias[2];
+
+	 magValue[0] = (float)sensor_theta1.ax - Magnetometer_offset[0];
+	 magValue[1] = (float)sensor_theta1.ay - Magnetometer_offset[1];
+	 magValue[2] = (float)sensor_theta1.az - Magnetometer_offset[2];
+
 
 	 if(status_flag_print == 10){
 	 status_flag_print = 0;
@@ -117,6 +133,78 @@ void interrupt_angles(){
 	 status_flag_print ++;}
 	 if(status_flag_diagnostic == 500){
 	 }
+}
+void Calibrar_Magnetometro(float *destination){
+    int i = 100;
+    int Max_Value[3];
+    int Min_Value[3];
+
+    DR_i2c_readraw(0,0x0C,0x03,7,leitura);
+    sensor_theta1.ax =      (leitura[1] << 8) | (leitura[0]); // HXL HXH
+    sensor_theta1.ay =      (leitura[3] << 8) | (leitura[2]);
+    sensor_theta1.az =      (leitura[4] << 8) | (leitura[4]);
+
+    Max_Value[0] = sensor_theta1.ax;
+	Min_Value[0] = sensor_theta1.ax;
+	Max_Value[1] = sensor_theta1.ay;
+	Min_Value[1] = sensor_theta1.ay;
+	Max_Value[2] = sensor_theta1.az;
+	Min_Value[2] = sensor_theta1.az;
+
+    while(i>0){
+		DR_i2c_readraw(0,0x0C,0x03,7,leitura);
+		sensor_theta1.ax =      (leitura[1] << 8) | (leitura[0]); // HXL HXH
+		sensor_theta1.ay =      (leitura[3] << 8) | (leitura[2]);
+		sensor_theta1.az =      (leitura[4] << 8) | (leitura[4]);
+			// Maior e menor valor no eixo X
+			if(sensor_theta1.ax > Max_Value[0]){
+				Max_Value[0] = sensor_theta1.ax;
+			}
+			if(sensor_theta1.ax < Min_Value[0]){
+				Min_Value[0] = sensor_theta1.ax;
+			}
+			// Maior e menor valor no eixo Y
+			if(sensor_theta1.ay > Max_Value[1]){
+				Max_Value[1] = sensor_theta1.ay;
+			}
+			if(sensor_theta1.ay < Min_Value[1]){
+				Min_Value[1] = sensor_theta1.ay;
+			}
+			// Maior e menor valor no eixo Z
+			if(sensor_theta1.az > Max_Value[2]){
+				Max_Value[2] = sensor_theta1.az;
+			}
+			if(sensor_theta1.az < Min_Value[2]){
+				Min_Value[2] = sensor_theta1.az;
+			}
+		DR_delay_k(0.01);
+		i--;
+    }
+	destination[0] = (Max_Value[0]+Min_Value[0])/2;	// Offset eixo x
+	destination[1] = (Max_Value[1]+Min_Value[1])/2; // Offset eixo y
+	destination[2] = (Max_Value[2]+Min_Value[2])/2; // Offset eixo z
+	
+
+}
+void initAK8963(float * destination)
+{
+  // First extract the factory calibration for each magnetometer axis
+  uint8_t rawData[3];  // x/y/z gyro calibration data stored here
+  DR_i2c_write(0,0x0C, 0x0A, 0x00); // Power down magnetometer  
+  DR_delay_k(1);
+  DR_i2c_write(0,0x0C, 0x0A, 0x0F); // Enter Fuse ROM access mode
+  DR_delay_k(1);
+  DR_i2c_readraw(0,0x0C,0X10,3,&rawData[0]); // Read the x-, y-, and z-axis calibration values
+  destination[0] =  (float)(rawData[0] - 128)/256. + 1.;   // Return x-axis sensitivity adjustment values, etc.
+  destination[1] =  (float)(rawData[1] - 128)/256. + 1.;  
+  destination[2] =  (float)(rawData[2] - 128)/256. + 1.; 
+  DR_i2c_write(0,0x0C, 0x0A, 0x00); // Power down magnetometer  
+  DR_delay_k(1);
+  // Configure the magnetometer for continuous read and highest resolution
+  // set Mscale bit 4 to 1 (0) to enable 16 (14) bit resolution in CNTL register,
+  // and enable continuous mode data acquisition Mmode (bits [3:0]), 0010 for 8 Hz and 0110 for 100 Hz sample rates
+  DR_i2c_write(0,0x0C, 0x0A, 1 << 4 | 0x02); // Set magnetometer data resolution and sample ODR
+  DR_delay_k(1);
 }
 
 int main(void)
@@ -138,23 +226,30 @@ int main(void)
 	DR_i2c_init(0);
 	DR_t32_init(0);
 	
-	DR_mpu6050_ligar(1000);
+	DR_mpu6050_ligar(10);
 	// status_erro = DR_mpu6050_read(sensor_rho.I2C, sensor_rho.address, 0x75, &data) - 10;
 	// // while (data != 0x68);
 	
-	// Set accelerometers low pass filter at 5Hz
+	// Set accelerometers low pass filter at 5Hz (ACCEL_CONFIG 2)
 	DR_i2c_write(sensor_rho.I2C, sensor_rho.address, 29, 0x06);
-	// Set gyroscope low pass filter at 5Hz
+	// Set gyroscope low pass filter at 5Hz (CONFIG)
 	DR_i2c_write(sensor_rho.I2C, sensor_rho.address, 26, 0x06);
 	
-	// Configure gyroscope range
+	// Configure gyroscope range(GYRO_FS_SEL)
 	DR_i2c_write(sensor_rho.I2C, sensor_rho.address,27, 0x10);
-	// Configure accelerometers range
+	// Configure accelerometers range (ACCEL_FS_SEL)
 	DR_i2c_write(sensor_rho.I2C, sensor_rho.address,28, 0x08);
 	// Set by pass mode for the magnetometers
 	DR_i2c_write(sensor_rho.I2C, sensor_rho.address,0x37, 0x02);
-	// Request continuous magnetometer measurements in 16 bits
+	// Request continuous magnetometer measurements in 16 bits !!!
 	DR_i2c_write(sensor_rho.I2C, 0x0C,0x0A, 0x16);
+	printf("\n\rSensores Inicializados");
+	// Auto Calibração do Magnetômetro
+	Calibrar_Magnetometro(Magnetometer_offset);
+    printf("\n\rSensores Calibrados");
+    printf("\n\rOffsetX : %.4f",Magnetometer_offset[0]);
+    printf("\n\rOffsetY : %.4f",Magnetometer_offset[1]);
+    printf("\n\rOffsetZ : %.4f",Magnetometer_offset[2]);
 	
 	/* Interrupt Config */
 	DR_uart_interrupt_receive();
@@ -170,9 +265,11 @@ int main(void)
 			printf("\n\n\r--Ledupdate(%d)",contador);
 			if (contador == 1)
 			{	/*Leitura do endereço*/
-				int j;
-				for (j = 0; j < 2; j++)
-				printf("\n\rRxData[%d]:%x", j, RXData[j]);
+				DR_i2c_read(0,0x68,0x75,RXData);
+				printf("\n\rMPU9250 i AM : 0x%x should be : 0x71",RXData[0]);
+				
+				DR_i2c_read(0,0x0C,0x00,RXData);
+				printf("\n\rMPU9250 i AM : 0x%x should be: 0X48",RXData[0]);
 			}
 			if (contador == 2)
 			{	/*Leitura acc e gyroscope*/
@@ -184,12 +281,14 @@ int main(void)
 				printf("\n\rleitura[%d]:%x", j, leitura[j]);
 				}
 			if (contador == 3)
-			{	/* Leitura Magnetometro endereço*/
+			{	/* Leitura Magnetometro calibração*/
+				float magCalibration[3];
+				initAK8963(magCalibration);
+				
+				printf("\n\rMag:X-Axis sensitivity adjustment value %.5f",magCalibration[0]);
+				printf("\n\rMag:Y-Axis sensitivity adjustment value %.5f",magCalibration[1]);
+				printf("\n\rMag:Z-Axis sensitivity adjustment value %.5f",magCalibration[2]);
 
-			    DR_i2c_read(0,0x0C,0X02,RXData);
-				int j;
-				for (j = 0; j < 2; j++)
-				printf("\n\rRxData[%d]:%x", j, RXData[j]);
 			}		
 			if (contador == 4)
 			{	/* Leitura Magnetometro data*/
